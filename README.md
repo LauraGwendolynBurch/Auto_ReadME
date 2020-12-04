5a83fe7f59b886efcf592e6fb1c1efabababcdb8
Laura Gwen <lauraburch.lb@gmail.com>
2020-12-04 14:31:06 -0500

    added readMe to followfor building the base of this project

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # Auto_ReadME
+
+## Description
+This code generates the base for a quality readMe file you can match to any project.  You will need to have text editor visual studio code, with node.js downloaded in order to run this.  Follow the promps in the terminal to fill in the content specific to your own project.  For complete step by step instructions follow the video provided in under the heading Test.
+
+## Table of Contents
+
+* [Title](#Title)
+
+* [Description](##Description)
+
+* [Link-to-project](##Link-to-project)
+
+* [Installation](##Installation)
+
+* [User-Story](##User-Story)
+
+* [License](##License)
+
+* [Contribution](##Contribution)  
+
+* [Test](##Test)
+
+* [Images](##Images)
+
+* [Credits](##Credits)
+
+* [Questions](##Questions)
+
+
+
+## Link-to-project 
+[Github Repo Auto_ReadMe](https://lauragwendolynburch.github.io/Auto_ReadME/)
+
+
+## Installation
+* NPM i inquirer
+* node.js
+
+## User-Story
+If you work with code this will save time while maintaining a quality readME.  
+
+## License
+[Apache License, Version 2.0](https://opensource.org/licenses/Apache-2.0)
+
+
+## Contribution
+Clone repo | Push changes | Create Pull request
+
+## Test
+![Sample Video of Project ](./images/sample1.png)
+
+## Images
+![Sample Image of Project ](./images/sample1.png)
+
+## Credits
+Anna Conover (tutor)
+
+LevelApp (tutor)
+
+University of Washington Bootcamp TAs and Instructor
+
+Study Group: Aaron Parnell, Keenan Reed, Sally Perez, Rattanak Leng
+
+## Questions
+GitHub Username: [LauraGwendolynBurch](https://github.com/LauraGwendolynBurch)
+
+Email: <lauraburch.lb@gmail.com>
+
+
--- index.js
+++ index.js
@@ -1,16 +1,71 @@
-// array of questions for user
-const questions = [
+// // array of questions for user
+// const questions = [
 
-];
+// ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
+
+inquirer
+    .prompt([{
+        type:'input',
+        message: "What is your project title?",
+        name: "title",
+    },
+    {
+        type:'input',
+        message: "Where is a description of this project?",
+        name: "description",
+    },
+    {
+        type:'input',
+        message: "What is your name?",
+        name: "personName",
+    },
+    {
+        type:'input',
+        message: "?",
+        name: "",
+    },
+    {
+        type:'input',
+        message: "",
+        name: "",
+    },
+    {
+        type:'input',
+        message: "What is your github URL for this project?",
+        name: "github",
+    },
+    ])
+
+    .then ( response => {
+        console.log(response.personName);
+        console.log(response.location);
+        console.log(response.linkedIn);
+        console.log(response.github);
+        const readmeFile = `
+             ${response.personName}
+             ${response.location}
+             ${response.linkedIn}
+             ${response.github}
+          `
+        const content = generateMarkdown(response);
+        fs.writeFile("README_GEN.md", readmeFile, (err) =>{ 
+            if (err)console.log("failed to write");
+            else console.log("wrote file")
+        });
+    });
